94c3fb419541d6056dd920dc29ecf9ad056818a6
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-08-01 00:03:53 -0400

    Create my user file

---
+++ users.py
@@ -0,0 +1,8 @@
+
+# Clase de Programacion 3, los Martes de 20:00 PM a 22:00 PM
+
+# Mi nombre es: Angel Antonio Gomera Romero
+# Matricula: 2022-0493
+
+# Este es un proyecto creado con FasatAPI, CRUD de usuarios con get, post, put y delete 
+
